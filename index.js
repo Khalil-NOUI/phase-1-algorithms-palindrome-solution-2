1ace6bf50615b2a7c9c1d6a546421549d53fcc92
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-08-30 16:39:04 +0100

    Completed assignments

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  // iterate from the beginning of the string to the middle of the string
+  for (let i = 0; i<(word.length)/2; i++){
+    let firstLetter = word[i];
+    let lastLetter = word[((word.length - 1) -i)]
+    //compare the letter we're iterating over to the corresponding letter at the end of the string
+    //if the letters don't match, return false
+    //if we reach the middle, and all the letters match, return true
+    return firstLetter === lastLetter;
 
+
+  }
+}
 /* 
-  Add your pseudocode here
+
+iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
+
 */
+
 
 /*
-  Add written explanation of your solution here
+
+  write a function that takes a string as input and check its first letter from the beginning against its first from the last and so on untill it reaches the Middle-letter of the string
+  the function return a bollean value depending on the final evaluation of the whole test. 
+
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+
+  //custom tests
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("4554"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("WOW"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("Mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("444"));
+
 
   console.log("");
 
